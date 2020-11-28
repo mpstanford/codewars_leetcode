10992b63370ccf7f3945ec5f1a0ccee3d2a4f23d
Madison Stanford <madison.stanford@gmail.com>
2020-11-28 12:33:11 -0800

    Implement House_Robber with Dynamic Programming

--- LeetCode/House_Robber.js
+++ LeetCode/House_Robber.js
@@ -1,22 +1,44 @@
+//Dynamic Programming Solution:
+
 var rob = function(nums) {
-    var currentMaxMoney = 0;
+  if (nums.length === 0) {
+    return 0;
+  } else if (nums.length === 1) {
+    return nums[0];
+  }
+  var runningTotal = [Math.max(nums[1], nums[0]), nums[0]];
 
-    var recurse = function(remaining, currentTotal) {
-        if (remaining.length <= 2) {
-            if (remaining.length === 2) {
-                currentTotal += Math.max(remaining[0], remaining[1]);
-            } else if (remaining.length === 1){
-                currentTotal += remaining[0]
-            }
-            currentMaxMoney = Math.max(currentTotal, currentMaxMoney);
-            return;
-        }
+  for (var i = 2; i < nums.length; i++) {
+    runningTotal.unshift(Math.max(nums[i] + runningTotal[1], runningTotal[0]));
+  }
 
-        recurse(remaining.slice(2), currentTotal + remaining[0]);
-        recurse(remaining.slice(3), currentTotal + remaining[1])
-
-    }
-
-    recurse(nums, 0);
-    return currentMaxMoney;
+  return Math.max(runningTotal[0], runningTotal[1]);
 };
+
+
+
+
+//Recursive Solution:
+
+// var rob = function(nums) {
+//     var currentMaxMoney = 0;
+
+//     var recurse = function(remaining, currentTotal) {
+//         if (remaining.length <= 2) {
+//             if (remaining.length === 2) {
+//                 currentTotal += Math.max(remaining[0], remaining[1]);
+//             } else if (remaining.length === 1){
+//                 currentTotal += remaining[0]
+//             }
+//             currentMaxMoney = Math.max(currentTotal, currentMaxMoney);
+//             return;
+//         }
+
+//         recurse(remaining.slice(2), currentTotal + remaining[0]);
+//         recurse(remaining.slice(3), currentTotal + remaining[1])
+
+//     }
+
+//     recurse(nums, 0);
+//     return currentMaxMoney;
+// };
