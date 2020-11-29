0befa9b97b642d8a23a3d279a10c22604e4edbb9
Madison Stanford <madison.stanford@gmail.com>
2020-11-29 07:34:31 -0800

    Implement Three_Sum

---
+++ LeetCode/Three_Sum.js
@@ -0,0 +1,34 @@
+var threeSum = function(nums) {
+  var allTriplets = [];
+  nums.sort((a,b) => {return a - b;});
+
+  for (var firstIndex = 0; firstIndex < nums.length - 2; firstIndex++) {
+    if (firstIndex > 0 && nums[firstIndex] === nums[firstIndex - 1]) {
+      continue;
+    }
+    var secondIndex = firstIndex + 1;
+    var thirdIndex = nums.length - 1;
+
+    while (secondIndex < thirdIndex) {
+      debugger;
+      var sum = nums[firstIndex] + nums[secondIndex] + nums[thirdIndex];
+
+      if (sum < 0) {
+        secondIndex++;
+      } else if (sum > 0) {
+        thirdIndex--;
+      } else {
+        allTriplets.push([nums[firstIndex], nums[secondIndex], nums[thirdIndex]]);
+        secondIndex++;
+        while (nums[secondIndex] === nums[secondIndex - 1] && secondIndex < thirdIndex) {
+          debugger;
+          secondIndex++;
+        }
+      }
+    }
+
+  }
+  return allTriplets;
+};
+
+console.log(threeSum([-1,0,1,2,-1,-4]));
