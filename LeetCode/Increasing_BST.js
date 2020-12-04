d39c11935b878f213062d38bcb57806dbde638f9
Madison Stanford <madison.stanford@gmail.com>
2020-12-03 18:55:49 -0800

    Start increasingBST

---
+++ LeetCode/Increasing_BST.js
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var increasingBST = function(root) {
+  //base case = left and right both undefined: return current tree
+
+  var traverseTree = function(currentNode, parentNode) {
+    //if currentNode === parentNode.right && currentNode.left !== null
+
+    //if currentNode.left !== null
+  }
+
+  traverseTree(root, null)
+  //if there's a left node, make the current node the right node of the left node
+
+  /
+};
